bafd14910c0d17a9426e13e63e4c1c2fd179f17d
bizet <176539309@qq.com>
2014-11-02 19:37:05 +0800

    change style

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require_once(dirname(__FILE__)."/src/vendor/macaw.php");
 require_once(dirname(__FILE__)."/src/user/user.route.php");
 
 Macaw::get('(:all)', function() {
-  echo require("static/index.html");
+  //header("Content-type:text/html");
+  //echo file_get_contents("static/index.html");
+  //readfile("static/index.html");
+  header('Location: client.html');
 });
 
 Macaw::dispatch();
